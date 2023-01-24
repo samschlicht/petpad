a1883c4d5c4f1da2e0a328c9c174bc9817b5918c
Isabel Figueroa Estrada <isabelfigueroaestrada@gmail.com>
2023-01-24 20:10:40 +0900

    creating a pet model

--- db/migrate/20230124110556_create_pets.rb
+++ db/migrate/20230124110556_create_pets.rb
@@ -1,10 +1,10 @@
 class CreatePets < ActiveRecord::Migration[7.0]
   def change
     create_table :pets do |t|
-      t.integer :user_id
       t.string :name
       t.string :species
-      t.text :comments
+      t.string :comments
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end

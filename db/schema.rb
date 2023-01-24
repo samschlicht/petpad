5852ed1d03d4fafafcba2bf014e3325ee7667f03
Isabel Figueroa Estrada <isabelfigueroaestrada@gmail.com>
2023-01-24 20:26:35 +0900

    Creating the booking models and its migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_24_112018) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_24_112529) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "number_of_pets"
+    t.date "start_date"
+    t.date "end_date"
+    t.string "comment"
+    t.string "status"
+    t.bigint "user_id", null: false
+    t.bigint "pad_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["pad_id"], name: "index_bookings_on_pad_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "pads", force: :cascade do |t|
     t.string "address"
@@ -51,6 +65,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_24_112018) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "pads"
+  add_foreign_key "bookings", "users"
   add_foreign_key "pads", "users"
   add_foreign_key "pets", "users"
 end

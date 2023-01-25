fbbe4f8aaaa84924ca18360d187a626372e7864b
Isabel Figueroa Estrada <isabelfigueroaestrada@gmail.com>
2023-01-26 07:30:07 +0900

    Creating the bookings controller and an empty new view for booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,19 @@
 class BookingsController < ApplicationController
+  before_action :set_pad, only: [:new, :create]
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.list = @pad
+    @booking.save
+    redirect_to pad_path(@pad)
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permi(:number_of_pets, :start_date, :end_date, :comment, :status, :user_id)
+  end
 end

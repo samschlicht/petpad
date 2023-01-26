0c7c32ea520695928a2d3503497ea224066835fe
Isabel Figueroa Estrada <isabelfigueroaestrada@gmail.com>
2023-01-26 22:06:11 +0900

    cheked the booking form

--- app/controllers/pads_controller.rb
+++ app/controllers/pads_controller.rb
@@ -23,12 +23,13 @@ class PadsController < ApplicationController
 
   def show
     @pad = Pad.find_by(id: params[:id])
+    authorize @pad
     if @pad.nil?
       flash[:alert] = "Pad not found"
       redirect_to pads_path
+
     end
   end
-
 
   def update
   end
@@ -41,5 +42,4 @@ class PadsController < ApplicationController
   def strong_params
     params.require(:pad).permit(:address, :title, :description, :price_per_night, :capacity, :availability, :photo)
   end
-
 end

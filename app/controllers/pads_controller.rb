feeff976a08ec813df9aab845688206a1b6b95b7
Sam Schlicht <samschlicht@gmail.com>
2023-02-04 14:58:21 +1100

    added address autocomplete to new pads form

--- app/controllers/pads_controller.rb
+++ app/controllers/pads_controller.rb
@@ -39,6 +39,7 @@ class PadsController < ApplicationController
   def index
     @padpictures = ['71MboshaisL.jpg', '71xnX1sKTTS._AC_SL1500_.jpg', '628ee6c06a5af245146626_w803h620.webp', '1000_F_275733004_ASmLOPFcapcxHg6xdui5B7DNc5HM2MD3.jpg', 'constructed-with-rot-resistant-fir-timber-requires-very-little-maintenance.jpg', 'Long-Plush-Dog-Bed-Pet-Blanket-Soft-Fleece-Cat-Cushion-Puppy-Chihuahua-Sofa-Mat-Pad-For_1080x.jpg', 'modern-dog-kennel-au.jpg', 's-l500.jpg', 'sloth-forest-scaled.jpg', 'Stacey-Cat-Enclosure.jpg', 'z61Dcak+uVPL.jpg', 'zc46aee_ea1a45a149da4446a2bbc8a2d0177d0a_mv2.jpg', 'ze2eec0f11dd7cca52b6601143f4f5f3b.jpg.optimal.jpg', 'zil_fullxfull.3756600185_gno4.jpg.webp', 'z111.jpg', 'zpurroom-little-chicken-series-milk-box-scratching-house-pawpawup-1_cleanup.jpg.webp', 'zs-l1600.jpg', 'zscale;webp=auto;jpeg_quality=60.jpg.webp', 'zUnknown.jpg']
     @pads = policy_scope(Pad).geocoded
+    @pads.where
     @markers = @pads.map do |pad|
       {
         lat: pad.latitude,

bedd90a1712b5f3945e5c79a73b9f509517c498b
James Chang <zhaoren@gmail.com>
2023-02-04 16:18:05 +0900

    pending is now default

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_31_105540) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_04_070925) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,7 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_31_105540) do
     t.date "start_date"
     t.date "end_date"
     t.string "comment"
-    t.string "status"
+    t.string "status", default: "pending"
     t.bigint "user_id", null: false
     t.bigint "pad_id", null: false
     t.datetime "created_at", null: false
